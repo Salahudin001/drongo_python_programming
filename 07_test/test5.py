b1e9c21d2d579879028da4e7d13258a000b51f88
salahudin001 <salahudinadan2069@gmail.com>
2023-02-28 16:55:56 +0300

    displays arguments

---
+++ 07_test/test5.py
@@ -0,0 +1,9 @@
+def function(*args):
+    for i in args:
+        print(i)
+
+function(5,25)
+function(20, 40, 60)
+function(80, 100)
+function(1,2,3,4,5,6)
+function('f','s','g','d','q','s','d','g','w')
