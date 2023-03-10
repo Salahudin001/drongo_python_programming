100ac0f74f16990d5c4c311e19f354f1ee448e6e
salahudin001 <salahudinadan2069@gmail.com>
2023-03-10 17:21:36 +0300

    dictionary

---
+++ 09_test/dictinary.py
@@ -0,0 +1,12 @@
+my_dict={}
+#creating a dictionary with integer keys
+fruits={'1':'apple','2':'banana','3':'cherry'}
+#creating a dictionary with mixed keys
+random_dict={'1':'red','Name':'Anushka'}
+print(my_dict)
+print(fruits)
+print(random_dict)
+print(fruits.keys())
+fruits.clear()
+
+print(fruits)
